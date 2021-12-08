26d570016bae5504afb877b1e3e69d010640716b
Patrick Mullen <patrickm@surveymonkey.com>
2021-12-08 10:26:44 -0800

    Working pressure plate!

--- objects/PressurePlate.gd
+++ objects/PressurePlate.gd
@@ -21,19 +21,20 @@ func _physics_process(delta):
 	for body in $KinematicBody2D/Area2D.get_overlapping_bodies():
 		weight += 1
 	var target_y = weight*2
+	print(target_y)
 	var k = $KinematicBody2D
-	k.position.y = target_y
-	var d = abs(k.position.y - target_y)
-	if d > 2:
-		if k.position.y < target_y:
-			move.y = 0.2
-		if k.position.y > target_y:
-			move.y = 0.2
-			print('going up', target_y)
-	k.position += move
-	if k.position.y > max_pressure:
-		k.position.y = max_pressure
-		move.y = 0
-	if k.position.y < 0:
-		k.position.y = 0
-		move.y = 0
+	#k.position.y = target_y
+	if k.position.y < target_y:
+		move.y = 2
+	if k.position.y > target_y:
+		move.y = -2
+	#k.position += move
+	var x = k.position.x
+	k.move_and_slide(move)
+	k.position.x = x
+	#if k.position.y > max_pressure:
+	#	k.position.y = max_pressure
+	#	move.y = 0
+	#if k.position.y < 0:
+	#	k.position.y = 0
+	#	move.y = 0
