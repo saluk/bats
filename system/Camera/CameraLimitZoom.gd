a3fe2b0f0d771b8705fe0b3281f9c9dd584fa85c
Patrick Mullen <patrickm@surveymonkey.com>
2022-03-01 13:12:42 -0800

    Simplify camera limits, timeout messages in the debuglogger

--- system/Camera/CameraLimitZoom.gd
+++ system/Camera/CameraLimitZoom.gd
@@ -24,42 +24,71 @@ func stable_lerp(cur_val, val, speed, dt):
 		return val
 	return cur_val
 	
-func find_world_camera_limits():
+func find_world_camera_limits_complex():
 	if not WorldSettings.room:
 		return
 	var cur_tile = WorldSettings.get_player_tile(bat.global_position)
 	var check_tile
 	
-	var left = cur_tile.x
+	var left = cur_tile.x-1
 	while 1:
-		check_tile = WorldSettings.connected_rooms.get(Vector2(left-1, cur_tile.y))
+		check_tile = WorldSettings.connected_rooms.get(Vector2(left, cur_tile.y))
+		DebugLogger.log_variable("left_tile:", check_tile)
 		if not check_tile or check_tile != WorldSettings.room.mapname:
 			break
 		left = left - 1
-	var right = cur_tile.x
+	var right = cur_tile.x+1
 	while 1:
-		check_tile = WorldSettings.connected_rooms.get(Vector2(right+1, cur_tile.y))
+		check_tile = WorldSettings.connected_rooms.get(Vector2(right, cur_tile.y))
+		DebugLogger.log_variable("right_tile:", check_tile)
 		if not check_tile or check_tile != WorldSettings.room.mapname:
 			break
 		right = right + 1
-	var top = cur_tile.y
+	var top = cur_tile.y-1
 	while 1:
-		check_tile = WorldSettings.connected_rooms.get(Vector2(cur_tile.x, top-1))
+		check_tile = WorldSettings.connected_rooms.get(Vector2(cur_tile.x, top))
+		DebugLogger.log_variable("top_tile:", check_tile)
 		if not check_tile or check_tile != WorldSettings.room.mapname:
 			break
 		top = top - 1
-	var bottom = cur_tile.y
+	var bottom = cur_tile.y+1
 	while 1:
-		check_tile = WorldSettings.connected_rooms.get(Vector2(cur_tile.x, bottom+1))
+		check_tile = WorldSettings.connected_rooms.get(Vector2(cur_tile.x, bottom))
+		DebugLogger.log_variable("bottom_tile:", check_tile)
 		if not check_tile or check_tile != WorldSettings.room.mapname:
 			break
 		bottom = bottom + 1
 
-	left = WorldSettings.local_position(Vector2(left*5*32, cur_tile.y), WorldSettings.room).x
-	right = WorldSettings.local_position(Vector2((right+1)*5*32, cur_tile.y), WorldSettings.room).x
-	top = WorldSettings.local_position(Vector2(cur_tile.x, top*4*32), WorldSettings.room).y
-	bottom = WorldSettings.local_position(Vector2(cur_tile.x, (bottom+1)*4*32), WorldSettings.room).y
+	left = WorldSettings.local_position(Vector2((left+1)*5*32, cur_tile.y), WorldSettings.room).x
+	right = WorldSettings.local_position(Vector2((right)*5*32, cur_tile.y), WorldSettings.room).x
+	top = WorldSettings.local_position(Vector2(cur_tile.x, (top+1)*4*32), WorldSettings.room).y
+	bottom = WorldSettings.local_position(Vector2(cur_tile.x, (bottom)*4*32), WorldSettings.room).y
+	DebugLogger.log_variable("bounds:",str(left)+","+str(right)+","+str(top)+","+str(bottom))
 	return {'left':left,'right':right,'top':top,'bottom':bottom}
+	
+func find_world_camera_limits_simple():
+	if not WorldSettings.room:
+		return
+	var left = null
+	var right = null
+	var top = null
+	var bottom = null
+	for tile in WorldSettings.room.get_used_cells():
+		if left == null or tile.x<left:
+			left = tile.x
+		if right == null or tile.x>right:
+			right = tile.x
+		if top == null or tile.y<top:
+			top = tile.y
+		if bottom == null or tile.y>bottom:
+			bottom = tile.y
+	DebugLogger.log_variable("right:", right)
+	return {
+		'left': left*32,
+		'right': right*32+32,
+		'top': top*32,
+		'bottom': bottom*32+32
+	}
 	
 func find_area_camera_limits():
 	for camera_region in get_tree().get_nodes_in_group("camera_region"):
@@ -87,9 +116,10 @@ func _process(delta):
 
 	if GlobalSettings.camera_limits:
 		# Find horizontal tiles used
-		var limits = find_area_camera_limits()
+		#var limits = find_area_camera_limits()
+		var limits
 		if not limits:
-			limits = find_world_camera_limits()
+			limits = find_world_camera_limits_simple()
 
 		if limits:
 			camera.limit_left = stable_lerp(camera.limit_left, limits.left, 250, delta)
@@ -104,7 +134,8 @@ func _process(delta):
 	var scaley = max(min(float(abs(camera.limit_top-camera.limit_bottom)) / 400.0, 2), 1)
 	var scale = min(scalex,scaley)
 
-	camera.zoom = lerp(camera.zoom, Vector2(
-		scale * zoom_scale,
-		scale * zoom_scale
-	), delta*2)
+	#camera.zoom = lerp(camera.zoom, Vector2(
+	#	scale * zoom_scale,
+	#	scale * zoom_scale
+	#), delta*2)
+	
--- system/debug/DebugLogger.gd
+++ system/debug/DebugLogger.gd
@@ -6,10 +6,29 @@ var tracked_variables = {
 var texts = []
 var dirty = true
 
+class Line extends Reference:
+	var l_type = ""
+	var l_name = ""
+	var l_value = ""
+	var l_time = 0
+	var l_pos = Vector2.ZERO
+	var timeout = null
+	func _init(type, name, value, time):
+		l_type = type
+		l_name = name
+		l_value = value
+		l_time = time
+		
+func timeout_lines():
+	for key in tracked_variables.keys():
+		if tracked_variables[key].timeout and Engine.get_frames_drawn() > tracked_variables[key].timeout:
+			tracked_variables.erase(key)
+
 func _process(_delta):
 	if dirty:
 		write_text()
 		dirty = false
+	timeout_lines()
 	update()
 
 func sort_custom(a, b):
@@ -31,35 +50,38 @@ func write_text():
 	for val in ar:
 		var name = val[0]
 		v = val[1]
-		s += name + ":" + str(v[0]) + "\n"
+		s += name + ":" + str(v.l_value) + "\n"
 	get_tree().get_nodes_in_group("debug_logger")[0].get_node("Control/Label").text = s
 	
 func _draw():
 	var v
 	for name in tracked_variables:
 		v = tracked_variables[name]
-		if v[1] == "vector":
+		if v.l_type == "vector":
 			draw_line(
-				v[0][0],
-				v[0][1],
+				v.l_value[0],
+				v.l_value[1],
 				Color.red,
 				1.0,
 				true
 			)
 
-func log_variable(name, value):
-	tracked_variables[name] = [value, "string", Engine.get_frames_drawn()]
+func log_variable(name, value, timeout = 0.1):
+	tracked_variables[name] = Line.new("string", name, value, Engine.get_frames_drawn())
+	if timeout:
+		tracked_variables[name].timeout = Engine.get_frames_drawn()+timeout/0.02
 	dirty = true
 	
 func log_increment(name):
 	if not name in tracked_variables:
-		tracked_variables[name] = ["0", "string", Engine.get_frames_drawn()]
-	tracked_variables[name][0] = str(int(tracked_variables[name][0])+1)
+		tracked_variables[name] = Line.new("string", name, "0", Engine.get_frames_drawn())
+	tracked_variables[name].l_value = str(int(tracked_variables[name].value)+1)
 	dirty = true
 
 func show_line(name, start_end_array):
-	tracked_variables[name] = [start_end_array, "vector", Engine.get_frames_drawn()]
+	tracked_variables[name] = Line.new("vector", name, start_end_array, Engine.get_frames_drawn())
 
 func show_at(name, value, pos):
 	# Not yet implemented
-	tracked_variables[name] = [value, "pos", pos]
+	tracked_variables[name] = Line.new("pos", name, value, Engine.get_frames_drawn())
+	tracked_variables[name].l_pos = pos
